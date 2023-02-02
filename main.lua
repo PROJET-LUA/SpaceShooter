2dec3142aba70ba665ca47c51ab5bf5cf3d719eb
Florian <florian.bernier@soait.fr>
2023-02-02 18:23:15 +0100

    v1

---
+++ main.lua
@@ -0,0 +1,63 @@
+
+
+------VARIABLE------
+------TABLEAU------
+------FONCTION LOAD------
+------FONCTION UPDATE------
+------FONCTION KEYPRESSED------
+------FONCTION MOUSEPRESSED------
+------FONCTION UTILE------
+
+-----LOAD----- : ACTION DU JEU AU DEMARAGE
+function love.load()
+end
+
+
+-----UPDATE----- : ACTION DU JEU A CHAQUE FRAME  
+function love.update(dt)
+
+
+
+    -----------------------------------
+    if love.mouse.isDown(1) then
+        print(love.mouse.getPosition())
+    end
+end
+
+
+-----DRAW----- : DESSINE CE QUE TU VOIS A L'ECRAN
+function love.draw()
+    love.graphics.rectangle("fill", 50, 50, 100, 100)
+end
+
+
+
+-----KEYPRESSED----- : ACTION DU JOUEUR CLAVIER
+function love.keypressed(key)
+    print(key) 
+end
+
+-----MOUSEPRESSED----- : ACTION DU JOUEUR SOURIS
+function love.mousepressed()
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
